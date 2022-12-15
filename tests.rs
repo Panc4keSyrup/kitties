fd6a4ee094f39310e4303e9a728c4542dfc1ee8c
Panc4keSyrup <116079124+Panc4keSyrup@users.noreply.github.com>
2022-12-15 13:39:56 +0700

    Update tests.rs

--- tests.rs
+++ tests.rs
@@ -6,8 +6,8 @@ fn is_the_right_kitty_created() {
 	new_test_ext().execute_with(|| {
 		let a =  [1u8];
 		let b = a.to_vec();
-		assert_ok!(Kitties::create_kitty(RuntimeOrigin::signed(1), b));
-		assert_eq!(Kitties::kitty_owned(1).to_vec(), b);
+		assert_ok!(Kitties::create_kitty(RuntimeOrigin::signed(1), b.clone());
+		assert_eq!(Kitties::kitty_owned(1).to_vec(), vec![b.clone()]);
 	});
 }
 #[test]
@@ -15,8 +15,8 @@ fn is_kitty_transfered() {
 	new_test_ext().execute_with(|| {
 		let a =  [1u8];
 		let b = a.to_vec();
-		assert_ok!(Kitties::create_kitty(RuntimeOrigin::signed(1), b));
-		assert_ok!(Kitties::transfer(RuntimeOrigin::signed(1), 2, b));
-		assert_eq!(Kitties::kitty_owned(2).to_vec(), b);
+		assert_ok!(Kitties::create_kitty(RuntimeOrigin::signed(1), b.clone()));
+		assert_ok!(Kitties::transfer(RuntimeOrigin::signed(1), 2, b.clone()));
+		assert_eq!(Kitties::kitty_owned(2).to_vec(), vec![b.clone()]);
 	});	
 }
