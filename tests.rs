0460c1f9c8164a8389e3d52fb1c7532fe5a67fcb
Panc4keSyrup <116079124+Panc4keSyrup@users.noreply.github.com>
2022-12-15 13:18:53 +0700

    Update tests.rs

--- tests.rs
+++ tests.rs
@@ -4,7 +4,7 @@ use frame_support::{assert_ok};
 #[test]
 fn is_the_right_kitty_created() {
 	new_test_ext().execute_with(|| {
-		let a =  1u8;
+		let a =  [1u8];
 		let b = a.to_vec();
 		assert_ok!(Kitties::create_kitty(RuntimeOrigin::signed(1), b));
 		assert_eq!(Kitties::kitty_owned(1).to_vec(), b);
